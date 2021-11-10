931fe7613087455ecbfd1bd797a6a906a5eeaaa9
martinmarinsalti <marinsaltimartin@gmail.com>
2021-11-09 23:15:49 -0300

    Spplication running correctly. Lack of ability to edit tasks

--- demo-1/src/main/java/com/example/demo/controller/TaskController.java
+++ demo-1/src/main/java/com/example/demo/controller/TaskController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.service.TaskService;
 
@@ -21,5 +24,31 @@ public class TaskController {
 		ArrayList<com.example.demo.entity.Task> task_list = ts.showTaskList();
 		model.addAttribute("task_list", task_list);
 		return "index.html";
+	}
+	
+	@PostMapping("/")
+	public String addTask(@RequestParam String nt) {
+		ts.loadTask(nt);
+		System.out.println("PASA");
+		return "redirect:/";
+	}
+	
+	@GetMapping("/{id}")
+	public String removeTask(@PathVariable("id") Integer id) {	
+		try {
+			ts.deleteTask(id);
+			return "redirect:/";
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return "redirect:/";
+		}
+	}
+		
+	@GetMapping("/")
+	public String editingTask(@PathVariable ("id") Integer idEd) {
+		ts.editTask(idEd);
+		return "redirect:/";
 	}
 }
--- demo-1/src/main/java/com/example/demo/service/TaskService.java
+++ demo-1/src/main/java/com/example/demo/service/TaskService.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 
+
+
 import java.util.ArrayList;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import com.example.demo.entity.Task;
 import com.example.demo.repository.TaskRepository;
@@ -20,5 +23,37 @@ public class TaskService {
 		ArrayList<Task> list = (ArrayList<Task>) tr.findAll();
 		System.out.println("La lista es "+list);
 		return list;
+	}
+	
+	@Transactional
+	public void deleteTask(Integer id) {
+		try {
+			tr.deleteById(id);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Transactional
+	public Task loadTask(String taskName) {
+
+		Task t = new Task();
+		t.setTASK(taskName);
+		t.setDO(false);
+		return tr.save(t);
+	}
+	
+	@Transactional
+	public void editTask(Integer id) {
+		Task t = tr.findById(id).get();
+		
+		if(t.getDO()) {
+			t.setDO(false);
+		}else {
+			t.setDO(true);
+		}
+		
+		tr.save(t);
 	}
 }
